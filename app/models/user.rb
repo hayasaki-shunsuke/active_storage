d85e623f36722aaaf3b69929779de40561fef1cc
hayasaki <hayasaki-shunsuke@pepabo.com>
2019-04-08 17:16:06 +0900

    不要なコメントアウトを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,15 +28,7 @@ class User < ApplicationRecord
     user
 
   end
-
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0,20]
-  #     user.name = auth.info.name
-  #   end
-  # end
-
+  
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}"
     # "#{auth.uid}-#{auth.provider}@pepabo.com"
